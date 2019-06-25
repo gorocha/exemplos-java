eb1d80c44600d00d3cd561a3de65909ae169a92b
gorocha <rochaguilherme355@gmail.com>
2019-06-25 11:21:59 -0300

    Atualização

---
+++ src/aula/Variaveis2.java
@@ -0,0 +1,16 @@
+package aula;
+
+import javax.swing.JOptionPane;
+
+public class Variaveis2 {
+
+	public static void main(String[] args) {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+		double d = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+		
+		System.out.println("i = " + i);
+		System.out.println("d = " + d);
+
+	}
+
+}
