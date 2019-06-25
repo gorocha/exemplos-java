9a764cec291826d7c5cc1f0f1b22a8deb3ee25c9
gorocha <rochaguilherme355@gmail.com>
2019-06-25 11:15:15 -0300

    Exercicio

---
+++ src/aula/Variaveis.java
@@ -0,0 +1,18 @@
+package aula;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		int i = 0;
+		float f = 10.3f;
+		double d = 10;
+		
+		System.out.println("a =" + a);
+		System.out.println("i = " + i);
+		System.out.println("f = " + d);
+		System.out.println("d = " + d);
+		
+	}
+
+}
